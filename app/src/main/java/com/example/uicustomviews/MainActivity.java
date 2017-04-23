9fc53b0319c5d67873438508d59f96fddccf3900
Danny <dannyann95@foxmail.com>
2017-04-23 11:09:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.uicustomviews;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //将系统自带的标题栏隐藏掉
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.hide();
+        }
+    }
+}
